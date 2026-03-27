49a6d4d730b4d74befdbeb4f0e753b1051fbeebc
Priyank Moradiya <153429763+Priyankm23@users.noreply.github.com>
2026-03-27 11:22:14 +0530

    fix: update CORS preflight request handling to use wildcard route

--- src/app.ts
+++ src/app.ts
@@ -47,7 +47,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 // Ensure preflight requests are handled
-app.options("*", cors(corsOptions));
+app.options("/*", cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
